e2f385fed7d37d4301b28ea8f77aa55048e5022b
MMAREK1 <martin.mat.marek@gmail.com>
2015-07-21 15:59:35 +0200

    initial commit
    initial commit

---
+++ js/hangman.js
@@ -0,0 +1,85 @@
+var secret ='JAVASCRIPT';
+var lettersGuessed='';
+var state=0;
+
+function getGuessedWord(secret, lettersGuessed)
+{
+  var knowsletters='';
+  for(var i=0;i<secret.length;i++)
+    {
+      if(lettersGuessed.indexOf(secret.charAt(i))>=0)
+        {
+          knowsletters+=secret.charAt(i);
+        }
+      else
+        knowsletters+='_';
+    }
+  return knowsletters;
+}
+function onClick(event)
+{
+  //deactive button
+  event.target.setAttribute('disabled','disabled');
+  if(secret.indexOf(event.target.textContent)<0)
+  {
+    state++;
+  }
+  var picture=document.getElementById('picture');
+  switch(state)
+  {
+    case 1:
+    picture.setAttribute('src','images/picture01.jpg');
+    break;
+    case 2:
+    picture.setAttribute('src','images/picture02.jpg');
+    break;
+    case 3:
+    picture.setAttribute('src','images/picture03.jpg');
+    break;
+    case 4:
+    picture.setAttribute('src','images/picture04.jpg');
+    break;
+    case 5:
+    picture.setAttribute('src','images/picture05.jpg');
+    break;
+    case 6:
+    picture.setAttribute('src','images/picture06.jpg');
+    alert('Prehral si.');
+    for(var btn of document.getElementById('alphabet'))
+    {
+      btn.setAttribute('disabled','disabled');
+    }
+    break;
+    default:
+    break;
+
+  }
+  lettersGuessed+=event.target.textContent;
+
+  var adress=document.getElementById('secret');
+  var returned=getGuessedWord(secret, lettersGuessed);
+  adress.textContent=returned;
+  if(returned.indexOf('_')<0)
+  {
+    alert('Vyhral si');
+        for(var btn of document.getElementById('alphabet'))
+    {
+      btn.setAttribute('disabled','disabled');
+    }
+  }
+}
+window.onload=function(){
+  var group=document.getElementById('alphabet');
+
+for(var i=65;i<91;i++)
+{
+  el=document.createElement('button');
+  el.setAttribute('type','button');
+  el.setAttribute('class','btn btn-primary');
+  el.addEventListener('click',onClick);
+  el.textContent=String.fromCharCode(i);
+
+  group.appendChild(el);
+
+}
+}
